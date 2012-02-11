a66932c5b5843741cfa74b79579b5a319d3b8cf5
Fabio Rodriguez <realgt@gmail.com>
2012-02-10 20:51:25 -0500

    removed unused imports

--- src/com/prosperent/api/service/ProsperentService.java
+++ src/com/prosperent/api/service/ProsperentService.java
@@ -1,7 +1,6 @@
 package com.prosperent.api.service;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.concurrent.ExecutionException;
@@ -10,8 +9,6 @@ import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpRequestRetryHandler;
-import org.apache.http.protocol.HttpContext;
 
 import android.net.http.AndroidHttpClient;
 import android.os.AsyncTask;
